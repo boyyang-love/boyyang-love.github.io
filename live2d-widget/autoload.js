cd9e298dadc3091eb2a9af9f9df2d38611072046
boyyang-love <boyyang-love@outlook.com>
2020-06-01 15:26:46 +0800

    Site updated: 2020-06-01 15:26:45

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -1,6 +1,6 @@
 // 注意：live2d_path 参数应使用绝对路径
 // 看板娘
-const live2d_path = "https://www.example.com/path/to/live2d-widget/";
+const live2d_path = "/live2d-widget/";
 // const live2d_path = "https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
 //const live2d_path = "/live2d-widget/";
 
